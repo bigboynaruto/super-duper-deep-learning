e63c42d9d0adbcbbeebd1aa565d357be6319578f
bigboynaruto <mailanton2@gmail.com>
2018-08-16 19:04:21 +0300

    added anime dataset

--- Restricted Boltzmann Machine/README.md
+++ Restricted Boltzmann Machine/README.md
@@ -1,2 +1,5 @@
 # Restricted Boltzmann Machine
 Pure NumPy (and a bit of SciPy) Binary Restricted Boltzmann Machine.
+
+# References
+Dataset: https://www.kaggle.com/CooperUnion/anime-recommendations-database
--- Restricted Boltzmann Machine/main.py
+++ Restricted Boltzmann Machine/main.py
@@ -1,25 +1,47 @@
 from rbm import RBM
 import numpy as np
+import pandas as pd
 
-# a very dumb dataset
-# either 100..0 or 011..1
-def get_simple_dataset(size=1000):
-	training_set = np.zeros(shape=(size, 10))
-	for row in training_set:
-		if np.random.randn() < 0.5:
-			row[1:] = 1
-		else:
-			row[0] = 1
+def main():
+	ratings = pd.read_csv('rating.csv', sep=',').values
+	# throws memory error
+	ratings = ratings[ratings[:,0] < 20000]
 
-	test_set = np.zeros(shape=(2,10))
-	test_set[0][0] = 1
-	test_set[1][1:] = 1
+	animes = pd.read_csv('anime.csv', sep=',', low_memory=False).values
 
-	return training_set, test_set
+	user_ids = np.unique(ratings[:,0])
+	anime_ids = np.unique(animes[:,0])
 
-training_set,test_set = get_simple_dataset()
+	user_count = len(user_ids)
+	anime_count = len(anime_ids)
 
-r = RBM(training_set.shape[1], 16)
-r.train(training_set, epochs=10, batch_size=10, k_steps=10)
+	user_id_scaler = {user_id:idx for user_id,idx in zip(user_ids, range(user_count))}
+	anime_id_scaler = {anime_id:idx for anime_id,idx in zip(anime_ids, range(anime_count))}
 
-print('Test loss: %s' % (r.predict(test_set)[1]))
+	print('Users: %d, Animes: %d' % (user_count, anime_count))
+
+	dataset = np.full((user_count, anime_count), -1)
+	for row in ratings:
+		# seems like ratings.csv contains invalid anime_id
+		try:
+			dataset[user_id_scaler[row[0]], anime_id_scaler[row[1]]] = row[2]
+		except:
+			pass
+
+	np.random.shuffle(dataset)
+
+	dataset[(dataset < 5) & (dataset >= 0)] = 0
+	dataset[dataset >= 5] = 1
+
+	training_set_size = dataset.shape[0] * 0.8
+	training_set,test_set = dataset[:training_set_size], dataset[training_set_size:]
+
+	print(training_set)
+
+	r = RBM(training_set.shape[1], 16)
+	r.train(training_set, epochs=10, batch_size=64, k_steps=1)
+
+	print('Test loss: %s' % (r.predict(test_set)[1]))
+
+if __name__ == '__main__':
+	main()
